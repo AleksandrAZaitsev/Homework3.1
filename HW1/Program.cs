3bbf7c3450f175f1441832303c3e9a7e6da0e519
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 17:24:19 +0500

    add HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,15 +1,21 @@
-﻿System.Console.Write("Введите первое число: ");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-System.Console.Write("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 if (numberA > numberB)
 {
-    System.Console.WriteLine(numberA);
+    System.Console.WriteLine("Максимальное число первое и равно " + numberA);
 }
 
 else
 {
-    System.Console.WriteLine(numberB);
+    System.Console.WriteLine("Максимальное число второе и равно " + numberB);
 }
