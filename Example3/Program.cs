df9e9606217383844eda46df7f32b9de5fcbdc5e
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 15:32:20 +0500

    add modify Example3

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -34,3 +34,44 @@ else
 {
     System.Console.WriteLine("Такого дня недели нет");
 }
+
+
+// или можно так
+// Console.Write("Введите номер дня недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// switch (day){
+//    case 1:{
+//        System.Console.WriteLine("Monday");
+//        break;
+//    }
+//    case 2:{
+//        System.Console.WriteLine("Tuesday");
+//        break;
+//    }
+//    case 3:{
+//        System.Console.WriteLine("Wednesday");
+//        break;
+//    }
+//    case 4:{
+//        System.Console.WriteLine("Thursday");
+//        break;
+//    }
+//    case 5:{
+//        System.Console.WriteLine("Friday");
+//        break;
+//    }
+//    case 6:{
+//        System.Console.WriteLine("Saturday");
+//        break;
+//    }
+//    case 7:{
+//        System.Console.WriteLine("Sunday");
+//        break;
+//    }
+//    default:
+//    {
+//        System.Console.WriteLine("Такого дня недели нет");
+//        break;
+//    }
+//}
