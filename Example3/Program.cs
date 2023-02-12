298d88c733ecdc2de8990d3a9c0830a9caed97fe
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 15:23:47 +0500

    add true Exaple3

---
+++ Example3/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1) 
+{
+    System.Console.WriteLine("Monday");
+}
+else if (day == 2)
+{
+    System.Console.WriteLine("Tuesday");
+}
+else if (day == 3)
+{
+    System.Console.WriteLine("Wednesday");
+}
+else if (day == 4)
+{
+    System.Console.WriteLine("Thursday");
+}
+else if (day == 5)
+{
+    System.Console.WriteLine("Friday");
+}
+else if (day == 6)
+{
+    System.Console.WriteLine("Saturday");
+}
+else if (day == 7)
+{
+    System.Console.WriteLine("Sunday");
+}
+
+else 
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
