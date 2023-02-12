60a2e38a014112e733943f40b643505006b2bdb5
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 16:15:19 +0500

    add HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    System.Console.WriteLine(numberA);
+}
+
+else
+{
+    System.Console.WriteLine(numberB);
+}
