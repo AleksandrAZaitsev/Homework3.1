0b98cea3482660e806095a4b2a6506cc3d8e7210
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 14:43:52 +0500

    modify Example1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -4,3 +4,6 @@ int square = numberA * numberA;
 
 Console.Write ("Квадрат равен: "); 
 Console.WriteLine (square); //cw
+
+//или Console.WriteLine ("Квадрат равен: "+ square);
+//или Console.WriteLine ($"Квадрат числа {numberA} равен: {square{}");
