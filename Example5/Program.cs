de27babbab1c5948818b624dc732562eb0bf5df9
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 16:09:04 +0500

    add Example5

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int i = number % 10;
+    System.Console.WriteLine(i);    
+}
+else
+{
+    System.Console.WriteLine("Введено не правильное число");
+}
