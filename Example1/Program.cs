63bd67ba782f81c3be12f6fb8c03b802c6a7803f
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 14:36:23 +0500

    square

---
+++ Example1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int square = numberA * numberA;
+
+Console.Write ("Квадрат равен: "); 
+Console.WriteLine (square); //cw
