2797bd36e8aac7bfcb0b5a958e817f6641937ece
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 15:56:24 +0500

    add Example4

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,6 +1,10 @@
 ﻿System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number <= 0)
+{
+    System.Console.WriteLine("Введите число больше 0");
+}
 for (int i = -number; i <= number; i++)
 {
     System.Console.Write(i + ", "); 
